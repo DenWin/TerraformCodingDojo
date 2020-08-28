d1283c0869c9223f1736f71d5350389fe123d1b5
DenWin <DenWin@users.noreply.github.com>
2020-08-28 09:17:23 +0200

    Day 2 - LAst Update

--- exercise_00/main.tf
+++ exercise_00/main.tf
@@ -12,10 +12,10 @@ provider "azurerm" {
   # the features block is mandatory, even if empty
   features {}
 
-  tenant_id       = "00000000-0000-0000-0000-000000000000
+  tenant_id       = "00000000-0000-0000-0000-000000000000"
   subscription_id = "00000000-0000-0000-0000-000000000000"
 
-  client_id       = "00000000-0000-0000-0000-000000000000
+  client_id       = "00000000-0000-0000-0000-000000000000"
   client_secret   = "client_secret"
 }
 
--- exercise_01/execute_with_var_parameters.sh
+++ exercise_01/execute_with_var_parameters.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Use your own tenant_id, subscription_id, client_id and client_secret.
-terraform plan -var="tenant_id=dec58247-1dd1-42b2-af3a-ca863318c424"       \
-               -var="subscription_id=84c5c22e-f0e6-4b99-99bb-98990facc06f" \
-               -var="client_id=dc046487-62ac-4524-bf67-85f0a50049d1"       \
+terraform plan -var="tenant_id=00000000-0000-0000-0000-000000000000"       \
+               -var="subscription_id=00000000-0000-0000-0000-000000000000" \
+               -var="client_id=00000000-0000-0000-0000-000000000000"       \
                -var="client_secret=client_secret"
